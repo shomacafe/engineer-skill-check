3d3d6fca4dafcafc7546ebd84323bf7efe7a4fcb
shomacafe <shoma21912@gmail.com>
2022-08-27 08:45:04 +0000

    [add] お知らせ機能の基本的なCRUD処理
    基本的なお知らせ機能を実装するため、articleコントローラのアクションとそれに対応するビューを追加しました。

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,12 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: %i(show edit update destroy)
 
   def index
     @articles = Article.all
   end
 
   def new
-    @articles = Article.new
+    @article = Article.new
   end
 
   def create
@@ -18,18 +19,32 @@ class ArticlesController < ApplicationController
     end
   end
 
+  def show
+  end
+
   def edit
   end
 
   def update
+    if @article.update(article_params)
+      redirect_to articles_url, notice: "お知らせ「#{@article.title}」を更新しました。"
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @article.destroy
+    redirect_to articles_url, notice: "お知らせ「#{@article.title}」を削除しました。"
   end
 
   private
 
   def article_params
-    params.require(:article).permit(:title, :content, :author, :employee_id)
+    params.require(:article).permit(:title, :content, :employee_id)
+  end
+
+  def set_article
+    @article = Article.find(params["id"])
   end
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -3,7 +3,6 @@ class Article < ApplicationRecord
 
   validates :title, presence: true
   validates :content, presence: true
-  validates :author, presence: true
 
   scope :active, -> {
     where(deleted_at: nil)
